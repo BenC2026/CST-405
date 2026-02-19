bb48cd2445cfcee024deeb0b9c63955f35fea636
BenC2026 <BCroyle1@my.gcu.edu>
2026-02-19 10:48:15 -0700

    Update symtab.h

--- symtab.h
+++ symtab.h
@@ -1,5 +1,6 @@
 #ifndef SYMTAB_H
 #define SYMTAB_H
+#define HASH_SIZE 211  // Prime number for better distribution
 
 /* SYMBOL TABLE
  * Tracks all declared variables during compilation
@@ -7,27 +8,42 @@
  * Used for semantic checking and code generation
  */
 
-#define MAX_VARS 100  /* Maximum number of variables supported */
+#define MAX_VARS 1000  /* Maximum number of variables supported */
 
 /* SYMBOL ENTRY - Information about each variable */
-typedef struct {
+typedef struct Symbol {
     char* name;     /* Variable identifier */
-    char* type;     /* Variable type */
+    char* type;     /* Variable type (e.g., "int") */
     int offset;     /* Stack offset in bytes (for MIPS stack frame) */
+    int isArray;   /* 1 if variable is an array, 0 otherwise */
+    int arraySize; /* Size of the array if isArray is 1 */
+    struct Symbol* next; /* For handling hash collisions (chaining) */
 } Symbol;
 
 /* SYMBOL TABLE STRUCTURE */
 typedef struct {
     Symbol vars[MAX_VARS];  /* Array of all variables */
+    Symbol* hashTable[HASH_SIZE]; /* Hash table for quick lookups */
     int count;              /* Number of variables declared */
     int nextOffset;         /* Next available stack offset */
+    int lookups;
+    int collisions;
 } SymbolTable;
+
+
+
+/* Global symbol table instance (defined in symtab.c) */
+extern SymbolTable symtab;
 
 /* SYMBOL TABLE OPERATIONS */
 void initSymTab();               /* Initialize empty symbol table */
 int addVar(char* name, char* type);          /* Add new variable, returns offset or -1 if duplicate */
 int getVarOffset(char* name);    /* Get stack offset for variable, -1 if not found */
 int isVarDeclared(char* name);   /* Check if variable exists (1=yes, 0=no) */
+int addArrayVar(char* name, char* type, int size); /* Add new array variable */
+int isArrayVar(char* name);      /* Check if variable is an array (1=yes, 0=no) */
+int getArraySize(char* name);    /* Get size of array */
 void printSymTab();              /* Print current symbol table contents for tracing */
+unsigned int hash(const char* str);          /* Hash function for variable names */
 
 #endif
